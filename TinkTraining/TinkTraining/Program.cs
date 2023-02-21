40989ed5f5ca245394416e1d1fd55592d74f404d
Dmitriy Listopad <dimalist2005@gmail.com>
2023-02-22 01:23:56 +0300

    Add project files.

---
+++ TinkTraining/TinkTraining/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int A, B, n;
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+        n = int.Parse(Console.ReadLine());
+        int[] arr = new int[3] { A,B,n};
+        
+    }
+}
